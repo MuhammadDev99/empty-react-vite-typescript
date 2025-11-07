973740566190a9a4434b1734158a6255c2915f14
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2025-11-08 00:30:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { signal } from "@preact/signals-react"
+import "./App.css"
+const count = signal<number>(0)
+function App() {
+    return (
+        <>
+            <div className="container">
+                <p>Hello, World!</p>
+                <button onClick={() => count.value++}>Count: {count}</button>
+            </div>
+        </>
+    )
+}
+
+export default App
