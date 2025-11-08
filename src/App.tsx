eb7673fe4a645966749bcae3df115842fb15920c
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2025-11-08 23:31:05 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const count = signal<number>(0)
 function App() {
     return (
         <>
+            <img className="cat" src="/images/cat.png" />
             <div className="container">
                 <p>Hello, World!</p>
                 <button onClick={() => count.value++}>Count: {count}</button>
