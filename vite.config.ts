9f5ae30453c26ca4f13032ace0641a7d1aa7cda4
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2025-11-08 20:42:38 +0300

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      // The plugin should be a simple string in the array
+      plugins: ["module:@preact/signals-react-transform"],
+    },
+  })],
 })
